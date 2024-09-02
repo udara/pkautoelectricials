ed0c4252114269002ed633ca8b28cc3592309695
Udara Ranasinghe <indi79@hotmail.com>
2024-09-03 08:59:51 +0930

    for production

--- next.config.js
+++ next.config.js
@@ -1,18 +1,18 @@
 // next.config.js
 
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export'
-};
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//     output: 'export'
+// };
+//
+// module.exports = nextConfig;
 
-module.exports = nextConfig;
-
-// module.exports = {
-//     images: {
-//         unoptimized: true,
-//     },
-//     assetPrefix: '/pkautocare/',  // Ensure this matches your GitHub Pages repository name
-//     basePath: '/pkautocare/',      // Ensure this matches your GitHub Pages repository name
-// }
+module.exports = {
+    images: {
+        unoptimized: true,
+    },
+    assetPrefix: '',  // Ensure this matches your GitHub Pages repository name
+    basePath: '',      // Ensure this matches your GitHub Pages repository name
+}
 
 //assetPrefix: process.env.NODE_ENV === 'production' ? '/' : '',
