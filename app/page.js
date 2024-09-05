828e9914de80d822e104dc146c0c28357ea02b87
Udara Ranasinghe <indi79@hotmail.com>
2024-09-05 19:09:05 +0930

    comming soon

--- app/page.js
+++ app/page.js
@@ -10,10 +10,18 @@ export const metadata = {
 const Home = () => {
     return (
         <>
-            <Hero/>
-            <AboutUs/>
-            <Testimonials/>
-            <FAQ/>
+            <div className="min-h-screen bg-gray-900 flex flex-col items-center justify-center">
+                <h1 className="text-5xl text-white font-bold mb-8 animate-pulse">
+                    Coming Soon
+                </h1>
+                <p className="text-white text-lg mb-8">
+                    We're working hard to bring you something amazing. Stay tuned!
+                </p>
+            </div>
+            {/*<Hero/>*/}
+            {/*<AboutUs/>*/}
+            {/*<Testimonials/>*/}
+            {/*<FAQ/>*/}
         </>
     );
 }
