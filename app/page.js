db47185fc2f574ebac2b41f0b3fc2c87248de705
Udara Ranasinghe <indi79@hotmail.com>
2024-09-05 19:11:25 +0930

    comming soon

--- app/page.js
+++ app/page.js
@@ -15,7 +15,7 @@ const Home = () => {
                     Coming Soon
                 </h1>
                 <p className="text-white text-lg mb-8">
-                    We're working hard to bring you something amazing. Stay tuned!
+                    We are working hard to bring you something amazing. Stay tuned!
                 </p>
             </div>
             {/*<Hero/>*/}
